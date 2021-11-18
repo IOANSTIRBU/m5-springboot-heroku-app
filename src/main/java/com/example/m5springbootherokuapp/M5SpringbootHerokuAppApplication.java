67cff0c95415b80886ea9887049bb645af7502bc
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-18 13:38:10 +0100

    initial commit

---
+++ src/main/java/com/example/m5springbootherokuapp/M5SpringbootHerokuAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.m5springbootherokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5SpringbootHerokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M5SpringbootHerokuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m5springbootherokuapp/M5SpringbootHerokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m5springbootherokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5SpringbootHerokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

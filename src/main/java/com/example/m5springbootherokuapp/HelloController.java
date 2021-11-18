9941369613dd3c198dcae68d16fbe93bd69079f0
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-18 13:49:30 +0100

    Controller Rest commit

---
+++ src/main/java/com/example/m5springbootherokuapp/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.m5springbootherokuapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Awesome app</title>
+                </head>
+                <body>
+                <h1>Home page</h1>
+                </body>
+                </html>
+                                
+                                
+                """;
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Awesome app</title>
+                </head>
+                <body>
+                <h1>Hello World page</h1>
+                </body>
+                </html>
+                """;
+    }
+
+
+}
